a4e74f6672fea9bc4a59967eb8421bc5981e3a46
Wallace Espindola <wallace.espindola@gmail.com>
2014-07-01 21:35:55 -0300

    Atualização de charset / encoding type, mudando para padrão UTF-8.

---
+++ ProjetoHibernate/src/br/telehand/util/OSState.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package br.telehand.util;
+
+/**
+ * @author W
+ *
+ */
+public abstract class OSState {
+
+	/**
+	 * 
+	 */
+	public OSState() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
