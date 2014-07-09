06f04ed0dbb589668e82502c81628ee4639316a6
adonai <adonaibm@gmail.com>
2014-07-09 15:53:30 -0300

    Ao realizar um UPDATE, automaticamente é atualizado o KIT

--- ProjetoHibernate/src/br/telehand/dao/AlmKitDAO.java
+++ ProjetoHibernate/src/br/telehand/dao/AlmKitDAO.java
@@ -6,11 +6,15 @@ import java.util.List;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.hibernate.Criteria;
 import org.hibernate.LockMode;
 import org.hibernate.Session;
 import org.hibernate.criterion.Example;
+import org.hibernate.criterion.Restrictions;
 
 import br.telehand.model.TbAlmKit;
+import br.telehand.model.TbAtendimento;
+import br.telehand.model.TbServico;
 import br.telehand.util.SessionFactorySingleton;
 /**
  * Home object for domain model class TbAlmKit.
@@ -122,6 +126,26 @@ public class AlmKitDAO {
 			return results;
 		} catch (RuntimeException re) {
 			log.error("find by example failed", re);
+			throw re;
+		}
+	}
+	
+	public TbAlmKit selecionarIdServico(int id) {
+		log.debug("getting TbAlmKit instance with id: " + id);
+		try {
+
+			Session session = SessionFactorySingleton.getSessionFactory().openSession();
+			Criteria cr = session.createCriteria(TbAlmKit.class);
+			TbAlmKit retorno = (TbAlmKit) cr.add( Restrictions.eq("idServico", id) ).uniqueResult();
+			
+			if (retorno == null) {
+				log.debug("Kit encontrado");
+			} else {
+				log.debug("Kit nao encontrado");
+			}
+			return retorno;
+		} catch (RuntimeException re) {
+			log.error("get failed", re);
 			throw re;
 		}
 	}
