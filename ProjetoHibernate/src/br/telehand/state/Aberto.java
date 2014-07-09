eb67c2f6728f193660b66f34e25537d265496758
Wallace Espindola <wallace.espindola@gmail.com>
2014-07-08 23:38:07 -0300

    Wallace: Implementado parcialmente padrão state

---
+++ ProjetoHibernate/src/br/telehand/state/Aberto.java
@@ -0,0 +1,33 @@
+package br.telehand.state;
+
+public class Aberto implements StateAtendimento {
+
+	public Aberto() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void reagendarAtendimento() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void concluirAtendimento() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void gerarPendencia() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void cancelarAtendimento() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ ProjetoHibernate/src/br/telehand/state/Efetuado.java
@@ -0,0 +1,33 @@
+package br.telehand.state;
+
+public class Efetuado implements StateAtendimento {
+
+	public Efetuado() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void reagendarAtendimento() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void concluirAtendimento() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void gerarPendencia() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void cancelarAtendimento() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
